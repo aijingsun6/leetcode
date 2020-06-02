aa59f18b9c6a6ca4c2741c1122c5eb1ced718fcb
alking <aijingsun6@gmail.com>
2020-06-02 20:55:19 +0800

    https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/25/

---
+++ src/main/java/org/alking/array/SingleNumber.java
@@ -0,0 +1,12 @@
+package org.alking.array;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for (int i = 0; i < nums.length; i++) {
+            num = num ^ nums[i];
+        }
+        return num;
+    }
+}
