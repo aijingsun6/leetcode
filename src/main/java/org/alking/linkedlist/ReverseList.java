8dc61e46422c21998f0bc008f4235b889cac516d
alking <alking@alking.cc>
2020-06-11 00:24:00 +0800

    optimise ReverseList.java

--- src/main/java/org/alking/linkedlist/ReverseList.java
+++ src/main/java/org/alking/linkedlist/ReverseList.java
@@ -4,35 +4,19 @@ public class ReverseList {
 
     public ListNode reverseList(ListNode head) {
 
-        if(head == null){
+        if (head == null) {
             // null
             return null;
         }
-        if(head.next == null){
-            // 1 element
-            return head;
-        }
-        if(head.next.next == null){
-            // 2 elements
-            ListNode result = head.next;
-            head.next = null;
-            result.next = head;
-            return result;
-        }
-        // > 2 elements
-
-        ListNode p1 = head.next.next;
-        ListNode p2 = head.next;
-        ListNode p3 = head;
-        // tail.next = null
-        head.next = null;
-        while (p1 != null){
-            p2.next = p3;
-            p3 = p2;
+        ListNode p1 = head;
+        ListNode p2 = null;
+        while (p1.next != null) {
+            ListNode t = p1.next;
+            p1.next = p2;
             p2 = p1;
-            p1 = p1.next;
+            p1 = t;
         }
-        p2.next = p3;
-        return p2;
+        p1.next = p2;
+        return p1;
     }
 }
