89ab286e56dc37cd8d4ab371a6e9282437e7a839
alking <aijingsun6@gmail.com>
2020-08-24 12:31:43 +0800

    P459

--- src/main/java/org/alking/p400/P459.java
+++ src/main/java/org/alking/p400/P459.java
@@ -18,27 +18,28 @@ public class P459 {
         int start = 0;
         int end = 0;
         boolean match = false;
-        int loopEnd = size/2;
+        int loopEnd = size / 2;
         for (int i = 1; i <= loopEnd; i++) {
             mod = size % i;
             if (mod == 0) {
                 repeat = size / i;
-                if(repeat < 2){
+                if (repeat < 2) {
                     break;
                 }
                 firstS = s.substring(0, i);
                 match = true;
+                start = 0;
                 for (int j = 1; j < repeat; j++) {
-                    start = i * j;
+                    start = start + i;
                     end = start + i;
-                    tmpS = s.substring(start,end);
-                    if(!tmpS.equals(firstS)){
+                    tmpS = s.substring(start, end);
+                    if (!tmpS.equals(firstS)) {
                         match = false;
                         break;
                     }
                 }
-                if(match){
-                    return match;
+                if (match) {
+                    return true;
                 }
             }
         }
