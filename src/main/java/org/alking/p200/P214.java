e7ce3e3bbefb9b6f079e310c6007bf498471dbfc
alking <alking@alking.cc>
2020-08-30 10:01:44 +0800

    P214

--- src/main/java/org/alking/p200/P214.java
+++ src/main/java/org/alking/p200/P214.java
@@ -1,53 +1,39 @@
 package org.alking.p200;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/shortest-palindrome/
  */
 public class P214 {
 
-    /**
-     * aacecaaa
-     * aaacecaaa
-     *
-     * @param s
-     * @return
-     */
     public String shortestPalindrome(String s) {
-
-        if (s == null || s.length() < 2) {
-            return s;
-        }
-        if (checkString(s)) {
-            return s;
-        }
-
-
-        for (int i = s.length() - 1; i > 0; i--) {
-
-            String s2 = s.substring(i, s.length());
-            s2 = new StringBuilder(s2).reverse().toString() + s;
-            if (checkString(s2)) {
-                return s2;
+        int n = s.length();
+        int[] fail = new int[n];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < n; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && s.charAt(j + 1) != s.charAt(i)) {
+                j = fail[j];
             }
-
+            if (s.charAt(j + 1) == s.charAt(i)) {
+                fail[i] = j + 1;
+            }
         }
-        return s;
+        int best = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            while (best != -1 && s.charAt(best + 1) != s.charAt(i)) {
+                best = fail[best];
+            }
+            if (s.charAt(best + 1) == s.charAt(i)) {
+                ++best;
+            }
+        }
+        String add = (best == n - 1 ? "" : s.substring(best + 1));
+        StringBuffer ans = new StringBuffer(add).reverse();
+        ans.append(s);
+        return ans.toString();
     }
 
-    private boolean checkString(String s) {
-        char[] arr = s.toCharArray();
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-
-            if (arr[start] != arr[end]) {
-                return false;
-            }
-            start += 1;
-            end -= 1;
-        }
-        return true;
-
-    }
 
 }
