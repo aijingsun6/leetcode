0de41c4cda4c4836426005112f48f1d5dec4352e
alking <aijingsun6@gmail.com>
2020-09-16 12:12:20 +0800

    P226

---
+++ src/main/java/org/alking/p200/P226.java
@@ -0,0 +1,26 @@
+package org.alking.p200;
+
+public class P226 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
