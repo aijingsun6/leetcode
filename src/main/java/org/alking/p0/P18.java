8bc089236e8b70d65d5820e58479eb9defed88a1
alking <alking@alking.cc>
2020-09-22 22:40:28 +0800

    P18

--- src/main/java/org/alking/p0/P18.java
+++ src/main/java/org/alking/p0/P18.java
@@ -2,61 +2,79 @@ package org.alking.p0;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class P18 {
 
 
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-
-        if (nums == null || nums.length < 4) {
-            return Collections.emptyList();
+    public List<List<Integer>> fourSum(int[] nums,int target){
+        /*定义一个返回值*/
+        List<List<Integer>> result=new ArrayList<>();
+        /*当数组为null或元素小于4个时，直接返回*/
+        if(nums==null||nums.length<4){
+            return result;
         }
-
-        List<List<Integer>> acc = new ArrayList<>();
-
+        /*对数组进行从小到大排序*/
         Arrays.sort(nums);
-        int size = nums.length;
-        // 0 ..    n-3, n-2,  n-1
-        for (int i = 0; i < size - 3; i++) {
-
-            for (int j = i + 1; j < size - 2; j++) {
-
-
-
-                int v = nums[i] + nums[j];
-
-                int left = j + 1;
-                int right = size - 1;
-
-                while (left < right) {
-
-                    if (left > (j +1) && nums[left] == nums[left - 1]) {
-                        left += 1;
-                        continue;
-                    }
-
-                    if (right < (size - 1) && nums[right] == nums[right + 1]) {
-                        right -= 1;
-                        continue;
-                    }
-
-                    int sum = v + nums[left] + nums[right];
-                    if (sum == target) {
-                        acc.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        left = left + 1;
-                        right = right - 1;
-                    } else if (sum > target) {
-                        right = right - 1;
-                    } else {
-                        left = left + 1;
-                    }
-
+        /*数组长度*/
+        int length=nums.length;
+        /*定义4个指针k，i，j，h  k从0开始遍历，i从k+1开始遍历，留下j和h，j指向i+1，h指向数组最大值*/
+        for(int k=0;k<length-3;k++){
+            /*当k的值与前面的值相等时忽略*/
+            if(k>0&&nums[k]==nums[k-1]){
+                continue;
+            }
+            /*获取当前最小值，如果最小值比目标值大，说明后面越来越大的值根本没戏*/
+            int min1=nums[k]+nums[k+1]+nums[k+2]+nums[k+3];
+            if(min1>target){
+                break;
+            }
+            /*获取当前最大值，如果最大值比目标值小，说明后面越来越小的值根本没戏，忽略*/
+            int max1=nums[k]+nums[length-1]+nums[length-2]+nums[length-3];
+            if(max1<target){
+                continue;
+            }
+            /*第二层循环i，初始值指向k+1*/
+            for(int i=k+1;i<length-2;i++){
+                /*当i的值与前面的值相等时忽略*/
+                if(i>k+1&&nums[i]==nums[i-1]){
+                    continue;
                 }
-
+                /*定义指针j指向i+1*/
+                int j=i+1;
+                /*定义指针h指向数组末尾*/
+                int h=length-1;
+                /*获取当前最小值，如果最小值比目标值大，说明后面越来越大的值根本没戏*/
+                int min=nums[k]+nums[i]+nums[j]+nums[j+1];
+                if(min>target){
+                    break;
+                }
+                /*获取当前最大值，如果最大值比目标值小，说明后面越来越小的值根本没戏，忽略*/
+                int max=nums[k]+nums[i]+nums[h]+nums[h-1];
+                if(max<target){
+                    continue;
+                }
+                /*开始j指针和h指针的表演，计算当前和，如果等于目标值，j++并去重，h--并去重，当当前和大于目标值时h--，当当前和小于目标值时j++*/
+                while (j<h){
+                    int curr=nums[k]+nums[i]+nums[j]+nums[h];
+                    if(curr==target){
+                        result.add(Arrays.asList(nums[k],nums[i],nums[j],nums[h]));
+                        j++;
+                        while(j<h&&nums[j]==nums[j-1]){
+                            j++;
+                        }
+                        h--;
+                        while(j<h&&i<h&&nums[h]==nums[h+1]){
+                            h--;
+                        }
+                    }else if(curr>target){
+                        h--;
+                    }else {
+                        j++;
+                    }
+                }
             }
         }
-        return acc;
+        return result;
     }
 }
