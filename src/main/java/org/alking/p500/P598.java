9753dc47628935f97174b72b1e3fd57040ee18cf
alking <alking@alking.cc>
2020-10-09 19:59:40 +0800

    P598

---
+++ src/main/java/org/alking/p500/P598.java
@@ -0,0 +1,15 @@
+package org.alking.p500;
+
+public class P598 {
+
+
+    public int maxCount(int m, int n, int[][] ops) {
+        int minM = m;
+        int minN = n;
+        for(int[] op: ops){
+            minM = Math.min(minM,op[0]);
+            minN = Math.min(minN,op[1]);
+        }
+        return minM * minN;
+    }
+}
