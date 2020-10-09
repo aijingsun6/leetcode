07f43ad53d2aa6271fee9e998bbcbff1147b1457
alking <alking@alking.cc>
2020-10-09 16:11:05 +0800

    P141

---
+++ src/main/java/org/alking/p100/P141.java
@@ -0,0 +1,32 @@
+package org.alking.p100;
+
+public class P141 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null){
+            if(fast == slow){
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
