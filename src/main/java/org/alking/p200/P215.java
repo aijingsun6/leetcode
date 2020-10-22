9e9956dcd14946539bb115b20e78bd92e7ae55c4
alking <alking@alking.cc>
2020-10-22 22:56:03 +0800

    P215

---
+++ src/main/java/org/alking/p200/P215.java
@@ -0,0 +1,27 @@
+package org.alking.p200;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class P215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int v : nums) {
+            int size = queue.size();
+            if (size < k) {
+                queue.offer(v);
+            } else {
+                int min = queue.peek();
+                if(v > min){
+                    queue.poll();
+                    queue.offer(v);
+                }
+            }
+        }
+        return queue.peek();
+    }
+}
