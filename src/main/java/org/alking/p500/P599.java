4cc7011730c36846ae80c280f3c64d8a82f4e840
alking <aijingsun6@gmail.com>
2020-11-02 13:03:06 +0800

    P599 v2

--- src/main/java/org/alking/p500/P599.java
+++ src/main/java/org/alking/p500/P599.java
@@ -1,11 +1,18 @@
 package org.alking.p500;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.*;
 
 public class P599 {
+
+    public static class SPos {
+        public String value;
+        public int idx;
+
+        public SPos(String value, int idx) {
+            this.value = value;
+            this.idx = idx;
+        }
+    }
 
 
     public String[] findRestaurant(String[] list1, String[] list2) {
@@ -17,28 +24,42 @@ public class P599 {
         if (list2 == null || list2.length == 0) {
             return new String[0];
         }
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
+
+        SPos[] sp1 = new SPos[list1.length];
         for (int i = 0; i < list1.length; i++) {
-            map1.put(list1[i], i);
-        }
-        for (int i = 0; i < list2.length; i++) {
-            map2.put(list2[i], i);
+            sp1[i] = new SPos(list1[i], i);
         }
 
-        String[] res = new String[list1.length];
+        SPos[] sp2 = new SPos[list2.length];
+        for (int i = 0; i < list2.length; i++) {
+            sp2[i] = new SPos(list2[i], i);
+        }
+
         int idx = 0;
         int idx1 = 0;
         int idx2 = 0;
-        Arrays.sort(list1);
-        Arrays.sort(list2);
-        while (idx1 < list1.length && idx2 < list2.length) {
+        Comparator<SPos> comparator = new Comparator<SPos>() {
+            @Override
+            public int compare(SPos o1, SPos o2) {
+                return o1.value.compareTo(o2.value);
+            }
+        };
+        Arrays.sort(sp1, comparator);
+        Arrays.sort(sp2, comparator);
 
-            String s1 = list1[idx1];
-            String s2 = list2[idx2];
-            int comp = s1.compareTo(s2);
+        PriorityQueue<SPos> queue = new PriorityQueue<>(new Comparator<SPos>() {
+            @Override
+            public int compare(SPos o1, SPos o2) {
+                return o1.idx - o2.idx;
+            }
+        });
+
+        while (idx1 < list1.length && idx2 < list2.length) {
+            SPos s1 = sp1[idx1];
+            SPos s2 = sp2[idx2];
+            int comp = s1.value.compareTo(s2.value);
             if (comp == 0) {
-                res[idx++] = s1;
+                queue.add(new SPos(s1.value, s1.idx + s2.idx));
                 idx1 += 1;
                 idx2 += 1;
             } else if (comp < 0) {
@@ -46,22 +67,12 @@ public class P599 {
             } else {
                 idx2 += 1;
             }
-
         }
-        TreeMap<Integer, ArrayList<String>> treeMap = new TreeMap<>();
-
-        for (int i = 0; i < idx; i++) {
-            String s = res[i];
-            int score = map1.get(s) + map2.get(s);
-            ArrayList<String> list = treeMap.getOrDefault(score, new ArrayList<>());
-            list.add(s);
-            treeMap.put(score, list);
+        ArrayList<String> res = new ArrayList<>();
+        int min = queue.peek().idx;
+        while (!queue.isEmpty() && queue.peek().idx == min) {
+            res.add(queue.poll().value);
         }
-        ArrayList<String> min = treeMap.firstEntry().getValue();
-        res = new String[min.size()];
-        for (int i = 0; i < min.size(); i++) {
-            res[i] = min.get(i);
-        }
-        return res;
+        return res.toArray(new String[res.size()]);
     }
 }
