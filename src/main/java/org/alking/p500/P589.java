93ec95b86c2d4171a12e29e6505e123ddec4d998
alking <alking@alking.cc>
2020-11-02 08:53:04 +0800

    P589

---
+++ src/main/java/org/alking/p500/P589.java
@@ -0,0 +1,45 @@
+package org.alking.p500;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P589 {
+
+    public static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<Integer> preorder(Node root) {
+        res = new ArrayList<>();
+        dfs(root);
+        return res;
+    }
+
+    private List<Integer> res;
+
+    private void dfs(Node root){
+        if(root == null){
+            return;
+        }
+        res.add(root.val);
+        List<Node> children = root.children;
+        if(children != null && !children.isEmpty()){
+            for(Node c: children){
+                dfs(c);
+            }
+        }
+
+    }
+}
