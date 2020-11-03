9007e2abc281fe359f04371671d64929be74168c
alking <aijingsun6@gmail.com>
2020-11-03 19:50:07 +0800

    P705 v2

--- src/main/java/org/alking/p700/P705.java
+++ src/main/java/org/alking/p700/P705.java
@@ -4,63 +4,23 @@ public class P705 {
 
     public static class MyHashSet {
 
-        public static class Node {
-            public int key;
-            public Node next;
+        public static final int MAX = 1000000;
 
-            public Node(int key, Node next) {
-                this.key = key;
-                this.next = next;
-            }
-        }
-
-        private static final int ARRAY_SIZE = 16;
-
-        private Node[] hash = new Node[ARRAY_SIZE];
+        private boolean[] arr = new boolean[MAX + 1];
 
         public MyHashSet() {
         }
 
         public void add(int key) {
-
-            int idx = Math.abs(key) % ARRAY_SIZE;
-            Node first = hash[idx];
-            Node node = hash[idx];
-            for (; node != null; node = node.next) {
-                if (node.key == key) {
-                    return;
-                }
-            }
-            Node n = new Node(key, first);
-            hash[idx] = n;
+            arr[key] = true;
         }
 
         public void remove(int key) {
-            int idx = Math.abs(key) % ARRAY_SIZE;
-            Node node = hash[idx];
-            Node last = null;
-            for (; node != null; last = node, node = node.next) {
-                if (node.key == key) {
-                    if (last == null) {
-                        //first node
-                        hash[idx] = node.next;
-                    } else {
-                        last.next = node.next;
-                    }
-                    return;
-                }
-            }
+            arr[key] = false;
         }
 
         public boolean contains(int key) {
-            int idx = Math.abs(key) % ARRAY_SIZE;
-            Node node = hash[idx];
-            for (; node != null; node = node.next) {
-                if (node.key == key) {
-                    return true;
-                }
-            }
-            return false;
+            return arr[key];
         }
     }
 }
