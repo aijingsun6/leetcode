7a0fe2d2ca617ae02149a240b579bce6e3d9113b
alking <aijingsun6@gmail.com>
2020-11-03 13:56:23 +0800

    P700

---
+++ src/main/java/org/alking/p700/P700.java
@@ -0,0 +1,25 @@
+package org.alking.p700;
+
+public class P700 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        }
+        return searchBST(root.right, val);
+    }
+}
