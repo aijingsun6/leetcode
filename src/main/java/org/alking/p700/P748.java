369a14fa7f0fa9712b8c0eaa01187266582b2d6d
alking <aijingsun6@gmail.com>
2020-11-04 19:23:22 +0800

    P748 v2

--- src/main/java/org/alking/p700/P748.java
+++ src/main/java/org/alking/p700/P748.java
@@ -30,6 +30,9 @@ public class P748 {
 
         String res = null;
         for (String word : words) {
+            if (res != null && word.length() >= res.length()) {
+                continue;
+            }
             boolean ok = true;
             int[] barr = buildCntArray(word);
             for (Integer idx : cs) {
