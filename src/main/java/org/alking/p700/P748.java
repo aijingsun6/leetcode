ee19c8a1f51ba09f3cd6ea228d07445eee826c0b
alking <aijingsun6@gmail.com>
2020-11-04 19:20:09 +0800

    P748 v1

---
+++ src/main/java/org/alking/p700/P748.java
@@ -0,0 +1,52 @@
+package org.alking.p700;
+
+import java.util.HashSet;
+
+public class P748 {
+
+    private int[] buildCntArray(String word) {
+        int[] arr = new int['z' - 'a' + 1];
+        for (char c : word.toCharArray()) {
+            c = Character.toLowerCase(c);
+            if (c >= 'a' && c <= 'z') {
+                int idx = c - 'a';
+                arr[idx] += 1;
+            }
+        }
+        return arr;
+    }
+
+
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] arr = buildCntArray(licensePlate);
+        HashSet<Integer> cs = new HashSet<>();
+        for (char c : licensePlate.toCharArray()) {
+            c = Character.toLowerCase(c);
+            if (c >= 'a' && c <= 'z') {
+                int idx = c - 'a';
+                cs.add(idx);
+            }
+        }
+
+        String res = null;
+        for (String word : words) {
+            boolean ok = true;
+            int[] barr = buildCntArray(word);
+            for (Integer idx : cs) {
+                if (barr[idx] < arr[idx]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                if (res == null) {
+                    res = word;
+                } else if (word.length() < res.length()) {
+                    res = word;
+                }
+            }
+        }
+        return res;
+
+    }
+}
---
+++ src/test/java/org/alking/p700/P748Test.java
@@ -0,0 +1,16 @@
+package org.alking.p700;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P748Test {
+
+    @Test
+    public void test(){
+        P748 p748 = new P748();
+        String licensePlate = "GAn3936";
+        String[] words = new String[]{"picture","community","stuff","above","be","around","can","father","avoid","agent"};
+        String res = p748.shortestCompletingWord(licensePlate,words);
+        Assert.assertEquals("agent",res);
+    }
+}
