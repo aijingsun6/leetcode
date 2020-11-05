d2bceda304c74597eb2d42375149f1f6cc301e2f
alking <aijingsun6@gmail.com>
2020-11-05 13:48:12 +0800

    P127 v2

--- src/main/java/org/alking/p100/P127.java
+++ src/main/java/org/alking/p100/P127.java
@@ -6,7 +6,6 @@ public class P127 {
 
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> wordSet = new HashSet<>(wordList);
         if (beginWord.length() != endWord.length()) {
             return 0;
         }
@@ -14,27 +13,63 @@ public class P127 {
         if (isLadder(beginWord, endWord)) {
             return 2;
         }
+
+        HashMap<String, ArrayList<String>> ladderMap = buildLadderMap(beginWord, wordList);
         int acc = 0;
-        ArrayList<String> queue = new ArrayList<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
         queue.add(beginWord);
+        HashSet<String> visit = new HashSet<>();
+        visit.add(beginWord);
 
         while (!queue.isEmpty()) {
-            ArrayList<String> list = queue;
-            queue = new ArrayList<>();
+            int size = queue.size();
             acc += 1;
-            wordSet.removeAll(list);
-            for (String poll : list) {
-                if (poll.equals(endWord)) {
+            for (int i = 0; i < size; i++) {
+                String poll = queue.poll();
+                if (endWord.equals(poll)) {
                     return acc;
                 }
-                for (String s : wordSet) {
-                    if (isLadder(poll, s)) {
-                        queue.add(s);
+                ArrayList<String> list = ladderMap.get(poll);
+                if (list != null && !list.isEmpty()) {
+                    for (String s : list) {
+                        if (!visit.contains(s)) {
+                            queue.add(s);
+                            visit.add(s);
+                        }
                     }
                 }
             }
         }
         return 0;
+    }
+
+    private HashMap<String, ArrayList<String>> buildLadderMap(String beginWord, List<String> wordList) {
+        HashMap<String, ArrayList<String>> acc = new HashMap<String, ArrayList<String>>();
+        int size = wordList.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                String s1 = wordList.get(i);
+                String s2 = wordList.get(j);
+                if (isLadder(s1, s2)) {
+                    ArrayList<String> l1 = acc.getOrDefault(s1, new ArrayList<>());
+                    l1.add(s2);
+                    acc.put(s1, l1);
+
+                    ArrayList<String> l2 = acc.getOrDefault(s2, new ArrayList<>());
+                    l2.add(s1);
+                    acc.put(s2, l2);
+                }
+            }
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (String s : wordList) {
+            if (isLadder(beginWord, s)) {
+                list.add(s);
+            }
+        }
+        acc.put(beginWord, list);
+        return acc;
+
     }
 
     private boolean isLadder(String from, String to) {
