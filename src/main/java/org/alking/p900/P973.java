c0f2ca6e536fd4426cb87a5e3ee4c9338b642f07
alking <aijingsun6@gmail.com>
2020-11-09 12:15:34 +0800

    P973 v1

---
+++ src/main/java/org/alking/p900/P973.java
@@ -0,0 +1,51 @@
+package org.alking.p900;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class P973 {
+
+    public static class Point {
+
+        public int x;
+        public int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int dist() {
+            return x * x + y * y;
+        }
+
+    }
+
+
+    public int[][] kClosest(int[][] points, int K) {
+
+        PriorityQueue<Point> queue = new PriorityQueue<>(new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return o2.dist() - o1.dist();
+            }
+        });
+
+        for (int[] p : points) {
+            queue.add(new Point(p[0], p[1]));
+            if (queue.size() > K) {
+                queue.remove();
+            }
+        }
+        int[][] res = new int[queue.size()][2];
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            Point p = queue.poll();
+            res[i] = new int[]{p.x, p.y};
+        }
+
+        return res;
+
+    }
+
+}
