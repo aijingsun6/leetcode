e96ad4ebc53f856b0a3dcd1bf517cab407bff8e5
alking <aijingsun6@gmail.com>
2020-11-20 12:54:58 +0800

    P147

---
+++ src/main/java/org/alking/p100/P147.java
@@ -0,0 +1,44 @@
+package org.alking.p100;
+
+public class P147 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        ListNode h = null;
+        ListNode tail = null;
+        while (head != null) {
+            ListNode node = head;
+            head = head.next;
+            if (tail == null) {
+                node.next = null;
+                h = node;
+                tail = node;
+                continue;
+            }
+            if (node.val < h.val) {
+                node.next = h;
+                h = node;
+            } else if (node.val >= tail.val) {
+                tail.next = node;
+                tail = node;
+                tail.next = null;
+            } else {
+                ListNode p = h;
+                while (p.next != null && node.val > p.next.val) {
+                    p = p.next;
+                }
+                node.next = p.next;
+                p.next = node;
+            }
+        }
+        return h;
+    }
+}
