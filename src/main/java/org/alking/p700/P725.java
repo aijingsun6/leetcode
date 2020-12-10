61b5a4af9cb1708da6db3b8a6a6587a14f12e740
alking <aijingsun6@gmail.com>
2020-12-10 13:26:03 +0800

    P725

---
+++ src/main/java/org/alking/p700/P725.java
@@ -0,0 +1,53 @@
+package org.alking.p700;
+
+public class P725 {
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        int size = size(root);
+        int len = size / k;
+        int left = size % k;
+        ListNode[] result = new ListNode[k];
+        ListNode dummy = new ListNode(0);
+        for (int i = 0; i < k; i++) {
+            if (root == null) {
+                break;
+            }
+            int count = len;
+            if (left > 0) {
+                left -= 1;
+                count += 1;
+            }
+            ListNode tail = dummy;
+            for (int j = 0; j < count && root != null; j++) {
+                tail.next = root;
+                tail = root;
+                root = root.next;
+            }
+            tail.next = null;
+            result[i] = dummy.next;
+        }
+        return result;
+
+    }
+
+    public int size(ListNode node) {
+
+        int acc = 0;
+        while (node != null) {
+            acc += 1;
+            node = node.next;
+        }
+        return acc;
+    }
+
+}
---
+++ src/test/java/org/alking/p700/P725Test.java
@@ -0,0 +1,22 @@
+package org.alking.p700;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P725Test {
+
+    @Test
+    public void test(){
+        P725.ListNode[] arr = new P725.ListNode[4];
+        for(int i = 0; i < arr.length;i++){
+            arr[i] = new P725.ListNode(i);
+            if(i > 0){
+                arr[i-1].next = arr[i];
+            }
+        }
+        P725 p725 = new P725();
+        P725.ListNode[] result = p725.splitListToParts(arr[0],5);
+        Assert.assertEquals(0,result[0].val);
+
+    }
+}
