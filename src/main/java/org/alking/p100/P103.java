6517ef8e11581c9ba24619ff02c2ad3c8c7c10a2
alking <alking@alking.cc>
2020-12-22 22:02:35 +0800

    P103

---
+++ src/main/java/org/alking/p100/P103.java
@@ -0,0 +1,48 @@
+package org.alking.p100;
+
+import java.util.*;
+
+public class P103 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        boolean right = true;
+        queue.addLast(root);
+        int size = 0;
+        while ((size = queue.size()) > 0) {
+            LinkedList<Integer> add = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode n = queue.pollFirst();
+                if (n.left != null) {
+                    queue.addLast(n.left);
+                }
+                if (n.right != null) {
+                    queue.addLast(n.right);
+                }
+                if (right) {
+                    add.addLast(n.val);
+                } else {
+                    add.addFirst(n.val);
+                }
+
+            }
+            result.add(add);
+            right = !right;
+        }
+        return result;
+    }
+}
