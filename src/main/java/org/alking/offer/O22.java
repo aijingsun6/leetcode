ef7afecde33ee0ab15242d51d91aa2eb128a8e9e
alking <aijingsun6@gmail.com>
2021-01-12 13:18:39 +0800

    O22

---
+++ src/main/java/org/alking/offer/O22.java
@@ -0,0 +1,28 @@
+package org.alking.offer;
+
+public class O22 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
