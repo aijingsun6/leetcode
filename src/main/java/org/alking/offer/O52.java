318778337df796e7ecf9d305172d1596f14c9f43
alking <aijingsun6@gmail.com>
2021-01-12 13:35:25 +0800

    O52

---
+++ src/main/java/org/alking/offer/O52.java
@@ -0,0 +1,51 @@
+package org.alking.offer;
+
+public class O52 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    private int size(ListNode head) {
+
+        int n = 0;
+        while (head != null) {
+            n += 1;
+            head = head.next;
+        }
+        return n;
+
+    }
+
+    private ListNode walk(ListNode head, int n) {
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+        return head;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        int sA = size(headA);
+        int sB = size(headB);
+        ListNode wA = headA;
+        ListNode wB = headB;
+        if (sA > sB) {
+            wA = walk(headA, sA - sB);
+        } else if (sB > sA) {
+            wB = walk(headB, sB - sA);
+        }
+
+        while (wA != wB) {
+            wA = wA.next;
+            wB = wB.next;
+        }
+        return wA;
+    }
+}
