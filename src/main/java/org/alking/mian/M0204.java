98dff85ee2b00d14e7526bfa253b1e83a25ef807
alking <alking@alking.cc>
2021-01-14 23:43:39 +0800

    M0204

---
+++ src/main/java/org/alking/mian/M0204.java
@@ -0,0 +1,57 @@
+package org.alking.mian;
+
+/**
+ * https://leetcode-cn.com/problems/partition-list-lcci/
+ */
+public class M0204 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode dummyL = new ListNode(0);
+        ListNode tailL = null;
+
+        ListNode dummyR = new ListNode(0);
+        ListNode tailR = null;
+
+        ListNode h = head;
+        while (h != null) {
+
+            if (h.val < x) {
+                if (tailL == null) {
+                    dummyL.next = h;
+                } else {
+                    tailL.next = h;
+                }
+                tailL = h;
+            } else {
+                if (tailR == null) {
+                    dummyR.next = h;
+                } else {
+                    tailR.next = h;
+                }
+                tailR = h;
+            }
+
+            h = h.next;
+        }
+
+        if (tailL != null) {
+            tailL.next = dummyR.next;
+        } else {
+            dummyL.next = dummyR.next;
+        }
+        if (tailR != null) {
+            tailR.next = null;
+        }
+        return dummyL.next;
+    }
+}
