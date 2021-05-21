8340983d79f5c0afca34a3c6e2b91d252602702f
alking <aijingsun6@gmail.com>
2021-05-21 18:57:27 +0800

    599

--- src/main/java/org/alking/p500/P599.java
+++ src/main/java/org/alking/p500/P599.java
@@ -1,21 +1,10 @@
 package org.alking.p500;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.List;
 
 public class P599 {
-
-    public static class SPos {
-        public String value;
-        public int idx;
-
-        public SPos(String value, int idx) {
-            this.value = value;
-            this.idx = idx;
-        }
-    }
 
 
     public String[] findRestaurant(String[] list1, String[] list2) {
@@ -28,54 +17,28 @@ public class P599 {
             return new String[0];
         }
 
-        SPos[] sp1 = new SPos[list1.length];
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < list1.length; i++) {
-            sp1[i] = new SPos(list1[i], i);
+            map.put(list1[i], i);
         }
 
-        SPos[] sp2 = new SPos[list2.length];
+
+        List<String> acc = new ArrayList<>();
+        int minScore = Integer.MAX_VALUE;
         for (int i = 0; i < list2.length; i++) {
-            sp2[i] = new SPos(list2[i], i);
-        }
-
-        int idx = 0;
-        int idx1 = 0;
-        int idx2 = 0;
-        Comparator<SPos> comparator = new Comparator<SPos>() {
-            @Override
-            public int compare(SPos o1, SPos o2) {
-                return o1.value.compareTo(o2.value);
-            }
-        };
-        Arrays.sort(sp1, comparator);
-        Arrays.sort(sp2, comparator);
-
-        PriorityQueue<SPos> queue = new PriorityQueue<>(new Comparator<SPos>() {
-            @Override
-            public int compare(SPos o1, SPos o2) {
-                return o1.idx - o2.idx;
-            }
-        });
-
-        while (idx1 < list1.length && idx2 < list2.length) {
-            SPos s1 = sp1[idx1];
-            SPos s2 = sp2[idx2];
-            int comp = s1.value.compareTo(s2.value);
-            if (comp == 0) {
-                queue.add(new SPos(s1.value, s1.idx + s2.idx));
-                idx1 += 1;
-                idx2 += 1;
-            } else if (comp < 0) {
-                idx1 += 1;
-            } else {
-                idx2 += 1;
+            String v = list2[i];
+            if (map.containsKey(v)) {
+                int score = map.get(v) + i;
+                if (score < minScore) {
+                    acc.clear();
+                    acc.add(v);
+                    minScore = score;
+                } else if (score == minScore) {
+                    acc.add(v);
+                }
             }
         }
-        ArrayList<String> res = new ArrayList<>();
-        int min = queue.peek().idx;
-        while (!queue.isEmpty() && queue.peek().idx == min) {
-            res.add(queue.poll().value);
-        }
-        return res.toArray(new String[res.size()]);
+
+        return acc.toArray(new String[0]);
     }
 }
