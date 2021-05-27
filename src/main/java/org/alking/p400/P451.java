6b53ef7e6d3f343d7bd674175f2e42bd8ab17b2d
alking <aijingsun6@gmail.com>
2021-05-27 13:11:37 +0800

    451

--- src/main/java/org/alking/p400/P451.java
+++ src/main/java/org/alking/p400/P451.java
@@ -1,33 +1,38 @@
 package org.alking.p400;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class P451 {
 
     public String frequencySort(String s) {
-
-        HashMap<Character, Integer> cntMap = new HashMap<>();
-        int N = s.length();
-        for (int i = 0; i < N; i++) {
-            char c = s.charAt(i);
-            cntMap.put(c, cntMap.getOrDefault(c, 0) + 1);
+        int size = 'z' - '0' + 1;
+        int[] bucket = new int[size];
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - '0';
+            bucket[idx]++;
         }
 
-        final PriorityQueue<Map.Entry<Character, Integer>> q = new PriorityQueue<Map.Entry<Character, Integer>>(new Comparator<Map.Entry<Character, Integer>>() {
+        PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
-            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-                return o2.getValue() - o1.getValue();
+            public int compare(int[] o1, int[] o2) {
+                return o2[1] - o1[1];
             }
         });
 
-        cntMap.entrySet().forEach(x -> q.add(x));
+        for (int i = 0; i < size; i++) {
+            if (bucket[i] > 0) {
+                q.offer(new int[]{i, bucket[i]});
+            }
+        }
 
-        char[] arr = new char[N];
-        int idx = 0;
+        char[] arr = new char[s.length()];
+        int offset = 0;
         while (!q.isEmpty()) {
-            Map.Entry<Character, Integer> e = q.poll();
-            Arrays.fill(arr, idx, idx + e.getValue(), e.getKey());
-            idx += e.getValue();
+            int[] e = q.poll();
+            Arrays.fill(arr, offset, offset + e[1], (char)('0' + e[0]));
+            offset += e[1];
         }
         return new String(arr);
     }
