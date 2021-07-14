649f6e5b39e77044e976d646f1b5f71fa0ba3e49
alking <aijingsun6@gmail.com>
2021-07-14 15:24:13 +0800

    1818

--- src/main/java/org/alking/p1800/P1818.java
+++ src/main/java/org/alking/p1800/P1818.java
@@ -1,16 +1,17 @@
 package org.alking.p1800;
 
-import java.util.TreeMap;
+import java.util.Arrays;
 
 public class P1818 {
 
     public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
         int N = nums1.length;
         final int MOD = 1000000007;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for (int v : nums1) {
-            map.put(v, 1);
-        }
+
+        int[] array = new int[N];
+        System.arraycopy(nums1, 0, array, 0, N);
+        Arrays.sort(array);
+
         long acc = 0;
         int v1;
         int v2;
@@ -21,25 +22,42 @@ public class P1818 {
             v2 = nums2[i];
             gap = Math.abs(v1 - v2);
             acc += gap;
-            if (map.containsKey(v2)) {
-                maxGap = Math.max(maxGap, gap);
-            } else {
-                Integer cellKey = map.ceilingKey(v2);
-                Integer floorKey = map.floorKey(v2);
-                int min = gap;
-                if (cellKey != null) {
-                    min = Math.min(min, cellKey - v2);
-                }
-                if (floorKey != null) {
-                    min = Math.min(min, v2 - floorKey);
-                }
-                maxGap = Math.max(maxGap, gap - min);
-            }
+            int min = findNearest(array, v2);
+            maxGap = Math.max(maxGap, gap - min);
 
         }
         acc = acc - maxGap;
         return (int) (acc % MOD);
     }
 
+    private int findNearest(int[] sortedArray, int value) {
+        int N = sortedArray.length;
+        int left = 0;
+        int right = N - 1;
+        if (value < sortedArray[left]) {
+            return sortedArray[left] - value;
+        }
+        if (value > sortedArray[right]) {
+            return value - sortedArray[right];
+        }
+        int min = Integer.MAX_VALUE;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int v = sortedArray[mid];
+            if (v > value) {
+                min = Math.min(min, v - value);
+                right = mid;
+            } else if (v < value) {
+                min = Math.min(min, value - v);
+                left = mid + 1;
+            } else {
+                return 0;
+            }
+        }
+        if (left == right) {
+            min = Math.min(min, Math.abs(sortedArray[left] - value));
+        }
+        return min;
+    }
 
 }
