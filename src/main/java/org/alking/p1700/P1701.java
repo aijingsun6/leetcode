c8c99a87bbefea2b31fc2935034f2b634e271d4d
alking <aijingsun6@gmail.com>
2021-07-15 13:27:57 +0800

    1701

--- src/main/java/org/alking/p1700/P1701.java
+++ src/main/java/org/alking/p1700/P1701.java
@@ -4,9 +4,9 @@ public class P1701 {
 
     public double averageWaitingTime(int[][] customers) {
 
-        int endTime = 0;
-        int waitSum = 0;
-        final long N = customers.length;
+        long endTime = 0;
+        long waitSum = 0;
+        final int N = customers.length;
         for (int[] customer : customers) {
             endTime = Math.max(endTime, customer[0]) + customer[1];
             waitSum += (endTime - customer[0]);
