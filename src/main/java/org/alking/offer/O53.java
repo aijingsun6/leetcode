b90e32d181fc13684a792cb40f30a2f034cf8c76
alking <aijingsun6@gmail.com>
2021-07-16 13:32:55 +0800

    O53

--- src/main/java/org/alking/offer/O53.java
+++ src/main/java/org/alking/offer/O53.java
@@ -1,47 +1,31 @@
 package org.alking.offer;
 
+
+import java.util.function.IntPredicate;
+
 public class O53 {
 
-    public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-        final int N = nums.length;
-        // find left
-        int leftPtr = 0;
+    private int partition(int[] nums, IntPredicate predicate) {
         int left = 0;
-        int right = N - 1;
+        int right = nums.length;
         int mid;
-        //找到第一个数字 = target
         while (left < right) {
             mid = left + (right - left) / 2;
-            if (nums[mid] < target) {
+            if (predicate.test(nums[mid])) {
                 left = mid + 1;
             } else {
                 right = mid;
             }
         }
-        leftPtr = left;
-        if(nums[leftPtr] != target){
+        return left;
+    }
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
-        //找到第一个数字 > target
-        left = 0;
-        right = N - 1;
-        int rightPtr = 0;
-        while (left < right) {
-            mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if(nums[left] > target){
-            rightPtr = left;
-        }else {
-            rightPtr = left + 1;
-        }
+        int leftPtr = partition(nums, (v) -> v < target);
+        int rightPtr = partition(nums, (v) -> v <= target);
         return rightPtr - leftPtr;
     }
 
