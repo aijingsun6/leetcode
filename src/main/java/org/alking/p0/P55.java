6b2f4796a6789a9e2b51bf653bedd73a929cb5c9
alking <aijingsun6@gmail.com>
2021-07-22 17:40:55 +0800

    55

--- src/main/java/org/alking/p0/P55.java
+++ src/main/java/org/alking/p0/P55.java
@@ -3,23 +3,16 @@ package org.alking.p0;
 public class P55 {
 
     public boolean canJump(int[] nums) {
-
-        final int N = nums.length;
-        final int target = N - 1;
-
-        boolean[] visit = new boolean[N];
-        visit[0] = true;
-
-        for (int i = 0; i < N; i++) {
-            if(visit[i]){
-                int start = i + 1;
-                int end = i + nums[i];
-                if (end >= target){
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
                     return true;
                 }
-                for(int j = start; j <= end;j++){
-                    visit[j] = true;
-                }
+            }else{
+                break;
             }
         }
         return false;
