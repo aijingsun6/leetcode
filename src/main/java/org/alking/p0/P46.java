bdbe73f16f6ca49c1fe9a9b4e0e6136d4b393e8b
alking <aijingsun6@gmail.com>
2021-07-22 12:33:38 +0800

    46

--- src/main/java/org/alking/p0/P46.java
+++ src/main/java/org/alking/p0/P46.java
@@ -1,5 +1,6 @@
 package org.alking.p0;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,9 +9,33 @@ import java.util.List;
 public class P46 {
 
     public List<List<Integer>> permute(int[] nums) {
-
-        return null;
+        this.result.clear();
+        final int N = nums.length;
+        this.visit = new boolean[N];
+        this.acc = new ArrayList<>();
+        backtrace(nums);
+        return this.result;
     }
 
+    private List<List<Integer>> result = new ArrayList<>();
 
+    private boolean[] visit;
+
+    private List<Integer> acc;
+
+    private void backtrace(int[] nums) {
+        if (nums.length == acc.size()) {
+            this.result.add(new ArrayList<>(acc));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(!visit[i]){
+                visit[i] = true;
+                acc.add(nums[i]);
+                backtrace(nums);
+                acc.remove(acc.size()-1);
+                visit[i] = false;
+            }
+        }
+    }
 }
