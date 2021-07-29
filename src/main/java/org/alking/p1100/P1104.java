f3eb8c19e72b14b36b516801622d86c7fb13c8d6
alking <aijingsun6@gmail.com>
2021-07-29 12:54:06 +0800

    1104

---
+++ src/main/java/org/alking/p1100/P1104.java
@@ -0,0 +1,37 @@
+package org.alking.p1100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class P1104 {
+
+    public List<Integer> pathInZigZagTree(int label) {
+        if(label < 2){
+            return Arrays.asList(1);
+        }
+        ArrayList<Integer> powArray = new ArrayList<>();
+        int p = 1;
+        while (p <= label) {
+            powArray.add(p);
+            p = p << 1;
+        }
+
+        List<Integer> path = new LinkedList<>();
+        int idx = powArray.size() - 1;
+        while (label > 0) {
+            path.add(0,label);
+            int pow = powArray.get(idx);
+            if(pow == 2){
+                label = 1;
+            }else{
+                label = (pow-1) - (label-pow)/2;
+            }
+            idx = idx - 1;
+        }
+        return path;
+    }
+
+
+}
