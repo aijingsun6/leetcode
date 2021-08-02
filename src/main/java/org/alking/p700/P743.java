cee2c4b94e67e134d49f012dff667043bd342383
alking <aijingsun6@gmail.com>
2021-08-02 12:39:19 +0800

    bfs

--- src/main/java/org/alking/p700/P743.java
+++ src/main/java/org/alking/p700/P743.java
@@ -1,5 +1,6 @@
 package org.alking.p700;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,41 +10,59 @@ public class P743 {
     public int networkDelayTime(int[][] times, int n, int k) {
         boolean[] visit = new boolean[n + 1];
         visit[0] = true;
-        int[] cost = new int[n + 1];
-        Arrays.fill(cost, -1);
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
         HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
-        for (int[] time : times) {
-            HashMap<Integer, Integer> m = map.getOrDefault(time[0], new HashMap<>());
-            m.put(time[1], time[2]);
-            map.put(time[0], m);
+        for (int[] t : times) {
+            HashMap<Integer, Integer> m = map.getOrDefault(t[0], new HashMap<>());
+            m.put(t[1], t[2]);
+            map.put(t[0], m);
         }
-        dfs(map, k, visit, cost, 0);
+
+        visit[k] = true;
+        dist[0] = 0;
+        dist[k] = 0;
+
+
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(k);
+
+        // bfs
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int p = queue.removeFirst();
+                int d = dist[p];
+                if (map.containsKey(p)) {
+                    HashMap<Integer, Integer> m = map.get(p);
+                    for (Map.Entry<Integer, Integer> entry : m.entrySet()) {
+                        int to = entry.getKey();
+                        int d2 = d + entry.getValue();
+                        if (!visit[to]) {
+                            visit[to] = true;
+                            dist[to] = d2;
+                            queue.addLast(to);
+                        } else if (d2 < dist[to]) {
+                            dist[to] = d2;
+                            queue.addLast(to);
+                        }
+                    }
+                }
+            }
+        }
+
         for (boolean v : visit) {
             if (!v) {
                 return -1;
             }
         }
         int result = 0;
-        for (int c : cost) {
+        for (int c : dist) {
             result = Math.max(result, c);
         }
         return result;
     }
 
-    private void dfs(final HashMap<Integer, HashMap<Integer, Integer>> map, int from, final boolean[] visit, final int[] cost, int acc) {
-
-        if (visit[from] && cost[from] < acc) {
-            return;
-        }
-        visit[from] = true;
-        cost[from] = acc;
-        if (map.containsKey(from)) {
-            HashMap<Integer, Integer> m = map.get(from);
-            for (Map.Entry<Integer, Integer> e : m.entrySet()) {
-                dfs(map, e.getKey(), visit, cost, acc + e.getValue());
-            }
-        }
-
-    }
 }
