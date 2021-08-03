546f72428919af5d3724e0abbcdc71f5fbb34f24
alking <aijingsun6@gmail.com>
2021-08-03 12:19:03 +0800

    581

--- src/main/java/org/alking/p500/P581.java
+++ src/main/java/org/alking/p500/P581.java
@@ -1,68 +1,42 @@
 package org.alking.p500;
 
-import java.util.Arrays;
-
 public class P581 {
 
     public int findUnsortedSubarray(int[] nums) {
 
-        if (nums == null || nums.length < 2) {
+        if (nums == null || nums.length < 1) {
             return 0;
         }
+        final int N = nums.length;
 
-        int[] arr = nums.clone();
-        Arrays.sort(arr);
-
-        int left = nums.length - 1;
-        int right = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (arr[i] != nums[i]) {
-                left = i;
-                break;
-            }
+        // find right ptr
+        int[] maxArray = new int[N];
+        int max = nums[0];
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, nums[i]);
+            maxArray[i] = max;
+        }
+        int rightPtr = N - 1;
+        while (rightPtr > 0 && nums[rightPtr] >= maxArray[rightPtr - 1]) {
+            rightPtr--;
         }
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (arr[i] != nums[i]) {
-                right = i;
-                break;
-            }
+        // find left ptr
+        int[] minArray = new int[N];
+        int min = nums[N - 1];
+        for (int i = N - 1; i >= 0; i--) {
+            min = Math.min(min, nums[i]);
+            minArray[i] = min;
+        }
+        int leftPtr = 0;
+        while (leftPtr < N - 1 && nums[leftPtr] <= minArray[leftPtr + 1]) {
+            leftPtr++;
         }
 
-        if(left >= right){
+        if (leftPtr >= rightPtr) {
             return 0;
         }
-        return right - left + 1;
-
+        return rightPtr - leftPtr + 1;
     }
 
-    private int findUnsortedSubarray(int[] nums, int start, int end) {
-
-        if (start >= end) {
-            return 0;
-        }
-        int min = nums[start];
-        int max = nums[end];
-        for (int idx = start; idx <= end; idx++) {
-            int v = nums[idx];
-            if (v > max) {
-                max = v;
-            }
-            if (v < min) {
-                min = v;
-            }
-        }
-
-        // find left
-        if (nums[start] == min) {
-            return findUnsortedSubarray(nums, start + 1, end);
-        }
-
-        if (nums[end] == max) {
-            return findUnsortedSubarray(nums, start, end - 1);
-        }
-
-
-        return end - start + 1;
-    }
 }
--- src/test/java/org/alking/p500/P581Test.java
+++ src/test/java/org/alking/p500/P581Test.java
@@ -11,5 +11,23 @@ public class P581Test {
         int[] nums = new int[]{2, 6, 4, 8, 10, 9, 15};
         int ret = p581.findUnsortedSubarray(nums);
         Assert.assertEquals(5,ret);
+
+        nums = new int[]{1,2,3,4};
+        ret = p581.findUnsortedSubarray(nums);
+        Assert.assertEquals(0,ret);
+
+
+        nums = new int[]{4,3,2,1};
+        ret = p581.findUnsortedSubarray(nums);
+        Assert.assertEquals(4,ret);
+
+        nums = new int[]{1};
+        ret = p581.findUnsortedSubarray(nums);
+        Assert.assertEquals(0,ret);
+
+
+        nums = new int[]{1,2,3,3,3};
+        ret = p581.findUnsortedSubarray(nums);
+        Assert.assertEquals(0,ret);
     }
 }
