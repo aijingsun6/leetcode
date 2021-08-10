5cdd1184436bdb5907b1262e93f238997e0172ef
alking <aijingsun6@gmail.com>
2021-08-10 12:49:37 +0800

    1353

--- src/main/java/org/alking/p1300/P1353.java
+++ src/main/java/org/alking/p1300/P1353.java
@@ -1,57 +1,36 @@
 package org.alking.p1300;
 
+
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class P1353 {
 
-    /**
-     * TODO
-     * @param events
-     * @return
-     */
     public int maxEvents(int[][] events) {
-
-        Arrays.sort(events, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                }
-                return o1[0] - o2[0];
+        //首先排序：开始时间小的在前。这样是方便我们顺序遍历，把开始时间一样的都放进堆
+        Arrays.sort(events, (o1, o2) -> o1[0] - o2[0]);
+        //小顶堆
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        //结果、开始时间、events下标、有多少组数据
+        int res = 0, last = 1, i = 0, n = events.length;
+        while (i < n || !pq.isEmpty()) {
+            //将start相同的会议都放进堆里
+            while (i < n && events[i][0] == last) {
+                pq.offer(events[i++][1]);
             }
-        });
-
-        int acc = 0;
-        int[] range = null;
-        for (int i = 0; i < events.length; i++) {
-            int[] e = events[i];
-            if (range == null) {
-                // 参加第一个会议 [e[0],e[0]]
-                range = e;
-                acc += 1;
-                continue;
+            //pop掉当前天数之前的
+            while (!pq.isEmpty() && pq.peek() < last) {
+                pq.poll();
             }
-
-
-            //     | ------ |
-            //     | ------ |
-
-
-            // | ---- |
-            //           |----|
-            if (e[0] > range[1]){
-                range = e;
-                acc += 1;
-                continue;
+            //顶上的就是俺们要参加的
+            if (!pq.isEmpty()) {
+                pq.poll();
+                res++;
             }
-
-
-
-
-
+            last++;
         }
-
-        return acc;
+        return res;
     }
+
+
 }
--- src/test/java/org/alking/p1300/P1353Test.java
+++ src/test/java/org/alking/p1300/P1353Test.java
@@ -16,5 +16,11 @@ public class P1353Test {
         events = new int[][]{{1,4},{4,4},{2,2},{3,4},{1,1}};
         result = p1353.maxEvents(events);
         Assert.assertEquals(4,result);
+
+        events = new int[][] {{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7}};
+        result = p1353.maxEvents(events);
+        Assert.assertEquals(7,result);
+
+
     }
 }
