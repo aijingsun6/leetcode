41045df8576eb2f040e1ca7c925c6b3c5a0da48c
alking <aijingsun6@gmail.com>
2021-08-11 16:07:27 +0800

    743

--- src/main/java/org/alking/p700/P743.java
+++ src/main/java/org/alking/p700/P743.java
@@ -1,9 +1,6 @@
 package org.alking.p700;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class P743 {
 
@@ -65,4 +62,119 @@ public class P743 {
         return result;
     }
 
+
+    /** 这种在这三种是最快的
+     * with priority queue
+     */
+    public int networkDelayTime2(int[][] times, int n, int k) {
+        int[][] graph = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(graph[i], -1);
+        }
+        for (int[] e : times) {
+            // e[0] -> e[1]
+            graph[e[0] - 1][e[1] - 1] = e[2];
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, -1);
+        dist[k - 1] = 0;
+        // [from,dist]
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        queue.offer(new int[]{k - 1, 0});
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            int from = p[0];
+            final int distAcc = p[1];
+            for (int to = 0; to < n; to++) {
+                if (graph[from][to] > -1) {
+                    int dist2 = distAcc + graph[from][to];
+                    if (dist[to] < 0 || dist2 < dist[to]) {
+                        dist[to] = dist2;
+                        queue.offer(new int[]{to, dist2});
+                    }
+                }
+            }
+
+        }
+        int max = 0;
+        for (int v : dist) {
+            if (v < 0) {
+                return v;
+            }
+            max = Math.max(max, v);
+        }
+        return max;
+    }
+
+    /**
+     * Bellman复杂度 (v * e)
+     */
+    public int networkDelayTimeBellman(int[][] times, int n, int k) {
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, -1);
+        dist[k] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int[] e : times) {
+                int from = e[0];
+                int to = e[1];
+                int price = e[2];
+                if (dist[from] > -1) {
+                    int acc = dist[from] + price;
+                    if (dist[to] < 0 || acc < dist[to]) {
+                        dist[to] = acc;
+                    }
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] < 0) {
+                return -1;
+            }
+            max = Math.max(max, dist[i]);
+        }
+        return max;
+    }
+
+    /**
+     * Dijkstra 复杂度 (v * v)
+     */
+    public int networkDelayTimeDijkstra(int[][] times, int n, int k) {
+        int[][] len = new int[n+1][n+1];
+        for (int i = 1; i < len.length; i++) {
+            for (int j = 1; j < len[i].length; j++) {
+                len[i][j] = i == j ? 0 : 0x3f3f3f3f;
+            }
+        }
+        for (int i = 0; i < times.length; i++) {
+            len[times[i][0]][times[i][1]] = times[i][2];
+        }
+        // 源k到其他节点的时长
+        int[] w = new int[n+1];
+        Arrays.fill(w, 0x3f3f3f3f);
+        w[k] = 0; // k到k为0
+        boolean[] via = new boolean[n+1]; // 是否确定到某节点的最小时长
+        for (int i = 1; i <= n; i++) {
+            int t = -1;
+            for (int j = 1; j <= n; j++) {
+                if (!via[j] && (t == -1 || w[j] < w[t])) t = j;
+            }
+            via[t] = true;
+            for (int j = 1; j <= n; j++) {
+                w[j] = Math.min(w[j], w[t] + len[t][j]);
+            }
+        }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            res = Math.max(res, w[i]);
+        }
+        return res >= 0x3f3f3f3f ? -1 : res;
+    }
 }
