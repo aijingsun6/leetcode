23fbd64c66eae309ec03879589230bf9f7fdca61
alking <alking@alking.cc>
2021-08-19 00:15:08 +0800

    1624

--- src/main/java/org/alking/p1600/P1624.java
+++ src/main/java/org/alking/p1600/P1624.java
@@ -1,6 +1,7 @@
 package org.alking.p1600;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class P1624 {
 
@@ -18,7 +19,26 @@ public class P1624 {
         for (int i = 0; i < bucket.length; i++) {
             int size = bucket[i].size();
             if (size > 1) {
-                max = Math.max(max, bucket[i].get(size-1) - bucket[i].get(0) - 1);
+                max = Math.max(max, bucket[i].get(size - 1) - bucket[i].get(0) - 1);
+            }
+        }
+        return max;
+    }
+
+    public int maxLengthBetweenEqualCharacters2(String s) {
+
+        int[] start = new int[26];
+        int[] end = new int[26];
+        Arrays.fill(start, -1);
+        Arrays.fill(end, -1);
+        int max = -1;
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if(start[idx]==-1){
+                start[idx] = i;
+            }else{
+                end[idx] = i;
+                max = Math.max(max,end[idx]-start[idx]-1);
             }
         }
         return max;
