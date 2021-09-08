2c7da3f7fffa6f37fc539dd90cc50ee9bdd3ccea
alking <aijingsun6@gmail.com>
2021-09-08 13:30:45 +0800

    v2

--- src/main/java/org/alking/p500/P502.java
+++ src/main/java/org/alking/p500/P502.java
@@ -40,11 +40,59 @@ public class P502 {
             if (find == null) {
                 break;
             }
-            for(int[] e: cache){
+            for (int[] e : cache) {
                 q.offer(e);
             }
             w += find[0];
             k -= 1;
+        }
+        return w;
+    }
+
+    public int findMaximizedCapital_V2(int k, int w, int[] profits, int[] capital) {
+        final int N = profits.length;
+        // int[profit,cap]
+        // cap <= w 的按照profit降序排列
+        PriorityQueue<int[]> left = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[0] - o1[0];
+            }
+        });
+
+        // int[profit,cap]
+        // cap > w 的按照cap 升序排列
+        PriorityQueue<int[]> right = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        // 操作步骤
+        // 1. 从left中pop 出来一个
+        // 2. 计算出w
+        // 3. 将right 中 cap <= 2中计算的w 的数据移到左边队列
+
+        for (int i = 0; i < N; i++) {
+            int[] e = new int[]{profits[i], capital[i]};
+            if (e[1] > w) {
+                right.offer(e);
+            } else {
+                left.offer(e);
+            }
+        }
+        while (k > 0 && !left.isEmpty()) {
+            int[] p = left.poll();
+            w += p[0];
+            while (!right.isEmpty()) {
+                p = right.peek();
+                if (p[1] > w) {
+                    break;
+                } else {
+                    left.offer(right.poll());
+                }
+            }
+            k--;
         }
         return w;
     }
--- src/test/java/org/alking/p500/P502Test.java
+++ src/test/java/org/alking/p500/P502Test.java
@@ -5,6 +5,12 @@ import org.junit.Test;
 
 public class P502Test {
 
+    P502 p502 = new P502();
+
+    private int findMaximizedCapital(int k, int w, int[] profits, int[] capital){
+        return p502.findMaximizedCapital_V2(k,w,profits,capital);
+    }
+
     @Test
     public void test(){
         P502 p502 = new P502();
@@ -12,14 +18,14 @@ public class P502Test {
         int w = 0;
         int[] profits = new int[]{1,2,3};
         int[] capital = new int[]{0,1,1};
-        int result = p502.findMaximizedCapital(k,w,profits,capital);
+        int result = findMaximizedCapital(k,w,profits,capital);
         Assert.assertEquals(4,result);
 
         k = 3;
         w = 0;
         profits = new int[]{1,2,3};
         capital = new int[]{0,1,2};
-        result = p502.findMaximizedCapital(k,w,profits,capital);
+        result = findMaximizedCapital(k,w,profits,capital);
         Assert.assertEquals(6,result);
 
 
@@ -27,7 +33,7 @@ public class P502Test {
         w = 2;
         profits = new int[]{1,2,3};
         capital = new int[]{1,1,2};
-        result = p502.findMaximizedCapital(k,w,profits,capital);
+        result = findMaximizedCapital(k,w,profits,capital);
         Assert.assertEquals(5,result);
 
 
