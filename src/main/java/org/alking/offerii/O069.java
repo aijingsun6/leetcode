0060734d2c5b2409af3730ca670e475bfcf19df9
alking <aijingsun6@gmail.com>
2021-10-14 12:21:01 +0800

    O069

--- src/main/java/org/alking/offerii/O069.java
+++ src/main/java/org/alking/offerii/O069.java
@@ -11,4 +11,18 @@ public class O069 {
         }
         return idx;
     }
+
+    public int peakIndexInMountainArray2(int[] arr) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
