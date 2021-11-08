0e9b2a3036b29c9666ef1c60e791259e359891e7
alking <aijingsun6@gmail.com>
2021-11-08 17:50:27 +0800

    299

--- src/main/java/org/alking/p200/P299.java
+++ src/main/java/org/alking/p200/P299.java
@@ -3,26 +3,25 @@ package org.alking.p200;
 public class P299 {
 
     public String getHint(String secret, String guess) {
-        int[] arr = new int[10];
+        int[] arr = new int['9' + 1];
         char[] ac1 = secret.toCharArray();
         char[] ac2 = guess.toCharArray();
         for (char c : ac1) {
-            arr[c - '0'] += 1;
+            arr[c] += 1;
         }
-        int same = 0;
+        int bulls = 0;
         int total = 0;
         for (int i = 0; i < ac1.length; i++) {
             char c1 = ac1[i];
             char c2 = ac2[i];
             if (c1 == c2) {
-                same += 1;
+                bulls++;
             }
-            int idx = c2 - '0';
-            if (arr[idx] > 0) {
-                total += 1;
-                arr[idx] --;
+            if (arr[c2] > 0) {
+                total++;
+                arr[c2]--;
             }
         }
-        return String.format("%dA%dB", same, total - same);
+        return String.format("%dA%dB", bulls, total - bulls);
     }
 }
