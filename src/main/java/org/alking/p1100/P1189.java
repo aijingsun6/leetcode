11e20a67830a8a551d0fa913e5147bdea8a5bce7
alking <alking@alking.cc>
2022-02-13 20:23:28 +0800

    1189

--- src/main/java/org/alking/p1100/P1189.java
+++ src/main/java/org/alking/p1100/P1189.java
@@ -3,16 +3,16 @@ package org.alking.p1100;
 public class P1189 {
 
     public int maxNumberOfBalloons(String text) {
-        int[] bucket = new int['z' - 'a' + 1];
-        for (int i = 0; i < text.length(); i++) {
-            bucket[text.charAt(i) - 'a']++;
+        int[] bucket = new int['z' + 1];
+        for (char c : text.toCharArray()) {
+            bucket[c]++;
         }
+        bucket['l'] /= 2;
+        bucket['o'] /= 2;
         final String balloon = "balon";
-        bucket['l' - 'a'] /= 2;
-        bucket['o' - 'a'] /= 2;
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < balloon.length();i++){
-            min = Math.min(min,bucket[balloon.charAt(i)-'a']);
+        for (char c : balloon.toCharArray()) {
+            min = Math.min(min, bucket[c]);
         }
         return min;
     }
