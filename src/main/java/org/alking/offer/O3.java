47c78d1863a91d86ae2e2134007633a7c288c29f
alking <alking@alking.cc>
2022-03-01 20:17:32 +0800

    offer

---
+++ src/main/java/org/alking/offer/O3.java
@@ -0,0 +1,23 @@
+package org.alking.offer;
+
+/**
+ * https://leetcode-cn.com/problems/string-to-url-lcci/
+ */
+public class O3 {
+
+    public String replaceSpaces(String S, int length) {
+        char[] cache = new char[S.length()];
+        int offset = 0;
+        for (int i = 0; i < length; i++) {
+            char c = S.charAt(i);
+            if (c == ' ') {
+                cache[offset++] = '%';
+                cache[offset++] = '2';
+                cache[offset++] = '0';
+            } else {
+                cache[offset++] = c;
+            }
+        }
+        return new String(cache,0,offset);
+    }
+}
