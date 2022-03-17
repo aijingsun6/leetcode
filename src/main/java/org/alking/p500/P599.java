cde82e480b337a4223e225b2b536ed38379f9051
alking <alking@alking.cc>
2022-03-17 21:13:06 +0800

    599

--- src/main/java/org/alking/p500/P599.java
+++ src/main/java/org/alking/p500/P599.java
@@ -6,39 +6,32 @@ import java.util.List;
 
 public class P599 {
 
-
     public String[] findRestaurant(String[] list1, String[] list2) {
-
-        if (list1 == null || list1.length == 0) {
-            return new String[0];
-        }
-
-        if (list2 == null || list2.length == 0) {
-            return new String[0];
-        }
-
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> idxMap = new HashMap<>();
         for (int i = 0; i < list1.length; i++) {
-            map.put(list1[i], i);
+            idxMap.put(list1[i], i);
         }
-
-
-        List<String> acc = new ArrayList<>();
-        int minScore = Integer.MAX_VALUE;
-        for (int i = 0; i < list2.length; i++) {
-            String v = list2[i];
-            if (map.containsKey(v)) {
-                int score = map.get(v) + i;
-                if (score < minScore) {
-                    acc.clear();
-                    acc.add(v);
-                    minScore = score;
-                } else if (score == minScore) {
-                    acc.add(v);
-                }
+        int min = Integer.MAX_VALUE;
+        List<Integer> idxList = new ArrayList<>();
+        for (int j = 0; j < list2.length; j++) {
+            String s = list2[j];
+            if (!idxMap.containsKey(s)) {
+                continue;
             }
-        }
+            int v = idxMap.get(s) + j;
+            if (v < min) {
+                min = v;
+                idxList = new ArrayList<>();
+                idxList.add(j);
+            } else if (v == min) {
+                idxList.add(j);
+            }
 
-        return acc.toArray(new String[0]);
+        }
+        String[] result = new String[idxList.size()];
+        for (int i = 0; i < idxList.size(); i++) {
+            result[i] = list2[idxList.get(i)];
+        }
+        return result;
     }
 }
