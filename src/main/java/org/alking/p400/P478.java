9e2eacdef6f33e1a9d73a7bcd1f8f65a285362e0
alking <alking@alking.cc>
2022-06-05 12:21:26 +0800

    961

---
+++ src/main/java/org/alking/p400/P478.java
@@ -0,0 +1,25 @@
+package org.alking.p400;
+
+import java.util.Random;
+
+public class P478 {
+
+    class Solution {
+        Random random;
+        double xc, yc, r;
+
+        public Solution(double radius, double x_center, double y_center) {
+            random = new Random();
+            xc = x_center;
+            yc = y_center;
+            r = radius;
+        }
+
+        public double[] randPoint() {
+            double u = random.nextDouble();
+            double theta = random.nextDouble() * 2 * Math.PI;
+            double r = Math.sqrt(u);
+            return new double[]{xc + r * Math.cos(theta) * this.r, yc + r * Math.sin(theta) * this.r};
+        }
+    }
+}
